cef521b81e797ad4ccf893afd5fa1eb9c631b264
Kevin Chaney <kevin_chaney@icloud.com>
2022-06-08 07:56:16 -0700

    commit tree 566ff22e56f6a0e42f38015ee8a6dfa128d4ae25

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+//Write a function that accepts a string as an argument
+//then the function takes that argument and checks if it reads the same forwards and backwards
+//the return value will be true if the word is a palindrome, and false if the word is not a palindrome
+//a palindrome is a word that is the same read forwards and backwards.
+//we only have to worry about lowercase letters and no special characters
+
+
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  let stringArr = word.split("").reverse().join("")
+  if(stringArr === word) {
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+define function isPalindrome with argument word
+  define constant stringArr to equal word with split method 
+  invoke reverse method on stringArr
+  invoke join method on stringArr
+  if stringArr is equal to word
+    return true
+  else
+    return false
 */
 
 /*
-  Add written explanation of your solution here
-*/
+  line 9: define isPalindrome function that passes argument word
+  line 10: stringArr undergoes the split method to turn the string into an array of elememts made of letters,
+    then stringArr undergoes the reverse method to reverse the order of the new array,
+    then stringArr undergoes the join method to turn the letter elements into a word
+  line 11-15: declare an if function that returns true if stringArr is equal to stringArrCopy, 
+    and returns false if stringArr is not equatl to stringArrCopy */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
